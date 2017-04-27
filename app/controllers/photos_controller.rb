ebffd00aefdd6429afdfc23d0e183b166a488300
seaneil01 <seaneil01@users.noreply.github.com>
2017-04-27 16:22:32 -0500

    working edit del

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,5 +1,6 @@
 class PhotosController < ApplicationController
   def index
+    @list_of_all_my_photos = Photo.all
     render("photos/index.html.erb")
   end
 
@@ -8,23 +9,46 @@ class PhotosController < ApplicationController
   end
 
   def create_row
-    render("photos/create_row.html.erb")
+    url = params[:source]
+    cap = params[:caption]
+    new_photo = Photo.new
+    new_photo.source = url
+    new_photo.caption = cap
+    new_photo.save
+    # render("photos/create_row.html.erb")
+    redirect_to("/photos")
   end
 
   def show
+    photo_number = params[:id].to_i
+    @user_photo = Photo.find(photo_number)
     render("photos/show.html.erb")
   end
 
   def edit_form
+    photo_number = params[:id].to_i
+    @user_photo = Photo.find(photo_number)
     render("photos/edit_form.html.erb")
   end
 
   def update_row
-    render("photos/update_row.html.erb")
+    photo_number = params[:id].to_i
+    url = params[:source]
+    cap = params[:caption]
+    edit_photo = Photo.find(photo_number)
+    edit_photo.source = url
+    edit_photo.caption = cap
+    edit_photo.save
+
+    redirect_to("/photos")
   end
 
   def destroy_row
-    render("photos/destroy_row.html.erb")
+
+    photo_number = params[:id].to_i
+    del_photo = Photo.find(photo_number)
+    del_photo.destroy
+    redirect_to("/photos")
   end
 
 end
