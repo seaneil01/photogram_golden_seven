fcad9b51cb9022cd2c8f87ea19ef6255db387f21
seaneil01 <seaneil01@users.noreply.github.com>
2017-04-27 14:05:13 -0500

    Class_work

---
+++ app/controllers/photos_controller.rb
@@ -0,0 +1,6 @@
+class PhotosController < ApplicationController
+  def new 
+        render("new.html.erb")
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get("/photos", {:controller => "photos", :actions => "photos"})
+
+  get("photos/new", {:controller => "photos", :action => "new"})
+
+  # photos/:photo
+  #
+  # photos/:photo/edit
+  #
+  # delete_photo/:photo
+
 end
