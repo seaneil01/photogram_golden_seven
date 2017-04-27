708d65179ab176ac8ad7ed4c63b7f4bb1055550b
seaneil01 <seaneil01@users.noreply.github.com>
2017-04-27 14:45:29 -0500

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get("/photos", {:controller => "photos", :actions => "photos"})
+  get("/photos", {:controller => "photos", :action => "index"})
 
-  get("photos/new", {:controller => "photos", :action => "new"})
+  get("photos/new", {:controller => "photos", :action => "new_form"})
 
-  # photos/:photo
-  #
-  # photos/:photo/edit
-  #
-  # delete_photo/:photo
+  get("create_photo", {:controller => "photos", :action => "create_row"})
 
+  get("photos/:id", {:controller => "photos", :action => "show"})
+
+  get("photos/:id/edit", {:controller => "photos", :action => "edit_form"})
+
+  get("update_photo/:id", {:controller => "photos", :action => "update_row"})
+
+  get("delete_photo/:id", {:controller => "photos", :action => "destroy_row"})
 end
